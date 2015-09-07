98e8c360d7e2df1ef01db59dc3ba723850e57e01
James <jkbennington@gmail.com>
2015-09-06 21:48:13 -0700

    added login paths

--- index.js
+++ index.js
@@ -16,7 +16,74 @@ app.get("/", function (req, res){
 	res.sendFile(path.join(views, 'index.html'));
 });
 
+app.use(
+	session({
+		secret: 'secret-key'
+		resave: false,
+		saveUninitialized: true
+	})
+);
+
+app.use(function(req,res,next){
+	req.login = function (user) {
+		req.session.userId = user._id;
+	};
+	req.currentUser = function(cb) {
+		db.User.findOne({_id:req.session.userId}m function (err, user){
+			req.user = user;
+			cb(null, user);
+		})
+	};
+	req.logout = function () {
+		req.session.userId = null;
+		req.user = null;
+	}
+	next();
+})
+
+app.get("/api/logout", function (req, res){
+	req.logout();
+	res.send(200);
+});
+
+app.post(["/users", "/api/signup"], function signup(req, res){
+	var user = req.body.user;
+	var email = user.email;
+	var password = user.password;
+	db.User.createSecure(email, password, function() {
+		db.User.authenticate(email, password, function (err, user){
+			req.login(user);
+			res.send(200);
+		});
+	});
+});
+
+app.post(["/sessions", "/api/login"], function login(req, res){
+	var user = req.body.user;
+	var email = user.email;
+	var password = user.password;
+	db>User.authenticate(email, password, function (err, user){
+		if(err){
+			res.redirect("/login")
+		}else {
+			req.login(user);
+			res.redirect(200)
+		}
+	})
+})
 
 app.listen(3000, function(){
 	console.log("Listening on port 3000.")
 });
+
+
+
+
+
+
+
+
+
+
+
+
