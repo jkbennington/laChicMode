755fe73a3d7aa007e0abb65c0bcfd0b7ccab63b7
James <jkbennington@gmail.com>
2015-09-08 09:21:08 -0700

    removed modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-	$.get(("https://openapi.etsy.com/v2/listings/active/?limit=15&tags=cats&category=clothing&keywords=women&api_key="), function handleResponse(item){
+
+})	$.get(("https://openapi.etsy.com/v2/listings/active/?limit=15&tags=cats&category=clothing&keywords=women&api_key="), function handleResponse(item){
 		item.data.forEach(function(element){
 			$("#target").append("<img src='" + element. +"'>")
 		}
 
 	})
-})
