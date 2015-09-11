ecfb6f8f1f121950fae0a0b9b1c0030b6ada77c0
James <jkbennington@gmail.com>
2015-09-11 00:10:36 -0700

    ready for heroku deploy attempt #1

--- index.js
+++ index.js
@@ -101,9 +101,7 @@ app.get("/profile", function userShow(req, res){
 });
 
 
-app.listen(3000, function(){
-	console.log("Listening on port 3000.")
-});
+app.listen(process.env.PORT || 3000)
 
 
 
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/laChicMode");
+mongoose.connect(process.env.MONGOLAB_URI ||
+				 process.env.MONGOHQ_URL ||
+				 "mongodb://localhost/laChicMode");
 module.exports.User = require("./user");
